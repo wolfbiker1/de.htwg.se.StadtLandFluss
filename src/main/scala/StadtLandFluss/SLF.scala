7369ec2db2d0fe49743d4b95f7bbf31872c3ba70
Benjamin Wilhelm <benjamin.wilhelm.1@gmx.de>
2020-08-21 19:30:36 +0200

    test

---
+++ src/main/scala/StadtLandFluss/SLF.scala
@@ -0,0 +1,7 @@
+package StadtLandFluss
+
+object SLF {
+  def main(args: Array[String]): Unit = {
+    System.out.printf("hi")
+  }
+}
--- src/main/scala/StadtLandFluss/scala.scala
+++
@@ -1,5 +0,0 @@
-package StadtLandFluss
-
-class scala {
-  
-}
