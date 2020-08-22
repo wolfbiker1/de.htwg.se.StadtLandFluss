69766092afd65b77b25eb1cea7ca15a4bdee7d09
Benjamin Wilhelm <benjamin.wilhelm.1@gmx.de>
2020-08-22 10:21:27 +0200

    build main structure

--- src/main/scala/de/htwg/se/stadtlandfluss/SLF.scala
+++ src/main/scala/de/htwg/se/stadtlandfluss/SLF.scala
@@ -1,7 +1,10 @@
 package de.htwg.se.stadtlandfluss
 
+import de.htwg.se.stadtlandflus.model.Player
+
 object SLF {
   def main(args: Array[String]): Unit = {
-    System.out.printf("hi")
+    val student = Player("Your Name")
+    println("Hello, " + student.name)
   }
 }
