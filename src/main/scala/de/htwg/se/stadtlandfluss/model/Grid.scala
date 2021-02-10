ef2fe802d748e6b622ee58eb0ea3318d47de493b
wolfbiker1 <be631wil@htwg-konstanz.de>
2021-02-10 11:57:00 +0100

    feat: remove house logic

--- src/main/scala/de/htwg/se/stadtlandfluss/model/Grid.scala
+++ src/main/scala/de/htwg/se/stadtlandfluss/model/Grid.scala
@@ -1,5 +1,12 @@
 package de.htwg.se.stadtlandfluss.model
 
-case class Grid() {
+import scala.math.sqrt
 
+case class Grid(private val cells:Matrix[Cell]) {
+  def this(size:Int) = this(new Matrix[Cell](size, Cell("")))
+  val size:Int = cells.size
+  def cell(row:Int, col:Int):Cell = cells.cell(row, col)
+  def set(row:Int, col:Int, value: String):Grid = copy(cells.replaceCell(row, col, Cell(value)))
+  
 }
+
