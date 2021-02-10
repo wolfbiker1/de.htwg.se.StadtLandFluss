60d7dd8f0e0634816b50ac1b744a7364c402cc43
wolfbiker1 <be631wil@htwg-konstanz.de>
2021-02-10 11:19:07 +0100

    feat: current style

--- src/main/scala/de/htwg/se/stadtlandfluss/model/Game.scala
+++
@@ -1,9 +0,0 @@
-package de.htwg.se.stadtlandfluss.model
-
-case class Game(board: Board) {
-
-
-  override def toString: String = {
-    "..."
-  }
-}
--- src/main/scala/de/htwg/se/stadtlandfluss/model/Grid.scala
+++ src/main/scala/de/htwg/se/stadtlandfluss/model/Grid.scala
@@ -1,5 +1,5 @@
 package de.htwg.se.stadtlandfluss.model
 
-case class Grid(board: Board) {
+case class Grid() {
 
 }
