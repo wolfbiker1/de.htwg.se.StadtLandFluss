5879759b4fcca2154b14b2a88145bff20ae85070
Helen <gaiserhelen@gmail.com>
2021-02-11 11:03:28 +0100

    test

---
+++ src/test/scala/de/htwg/se/stadtlandfluss/modul/MatrixSpec.scala
@@ -0,0 +1,36 @@
+package de.htwg.se.stadtlandfluss.modul
+
+import de.htwg.se.stadtlandfluss.model.{Cell, Matrix}
+import org.scalatest.{Matchers, WordSpec}
+
+class MatrixSpec extends WordSpec with Matchers {
+  "A Matrix is a tailor-made immutable data type that contains a two-dimentional Vector of Cells. A Matrix" when {
+    "empty " should {
+      "be created by using a dimention and a sample cell" in {
+        val matrix = new Matrix[Cell](2, Cell(""))
+        matrix.size should be(2)
+      }
+      "for test purposes only be created with a Vector of Vectors" in {
+        val testMatrix = Matrix(Vector(Vector(Cell(""))))
+        testMatrix.size should be(1)
+      }
+
+    }
+    "filled" should {
+      val matrix = new Matrix[Cell](2, Cell(""))
+      "give access to its cells" in {
+        matrix.cell(0, 0) should be(Cell(""))
+      }
+      "replace cells and return a new data structure" in {
+        val returnedMatrix = matrix.replaceCell(0, 0, Cell(""))
+        matrix.cell(0, 0) should be(Cell(""))
+        returnedMatrix.cell(0, 0) should be(Cell(""))
+      }
+      "be filled using fill operation" in {
+        val returnedMatrix = matrix.fill(Cell(""))
+        returnedMatrix.cell(0,0) should be(Cell(""))
+      }
+    }
+  }
+
+}
