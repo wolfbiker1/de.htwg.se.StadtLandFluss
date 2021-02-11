b87fd2c38bad067c74fe20fec629fe77edc1057a
Helen <gaiserhelen@gmail.com>
2021-02-11 08:59:41 +0100

    tests

--- src/test/scala/de/htwg/se/stadtlandfluss/modul/PlayerSpec.scala
+++ src/test/scala/de/htwg/se/stadtlandfluss/modul/PlayerSpec.scala
@@ -1,5 +1,18 @@
 package de.htwg.se.stadtlandfluss.modul
 
-class PlayerSpec {
+import de.htwg.se.stadtlandfluss.model.Player
+import org.scalatest._
+
+class PlayerSpec extends WordSpec with Matchers{
+
+  "A Player" when { "new" should {
+    val player = Player("Your Name")
+    "have a name"  in {
+      player.name should be("Your Name")
+    }
+    "have a nice String representation" in {
+      player.toString should be("Your Name")
+    }
+  }}
 
 }
