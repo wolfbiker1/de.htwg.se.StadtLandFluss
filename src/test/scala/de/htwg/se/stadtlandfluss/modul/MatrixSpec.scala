ffe7d34e5928bb8214bdb9edcda0be1031a15259
wolfbiker1 <be631wil@htwg-konstanz.de>
2021-02-15 12:47:36 +0100

    feat: adapt tests

--- src/test/scala/de/htwg/se/stadtlandfluss/modul/MatrixSpec.scala
+++ src/test/scala/de/htwg/se/stadtlandfluss/modul/MatrixSpec.scala
@@ -7,17 +7,17 @@ class MatrixSpec extends WordSpec with Matchers {
   "A Matrix is a tailor-made immutable data type that contains a two-dimentional Vector of Cells. A Matrix" when {
     "empty " should {
       "be created by using a dimention and a sample cell" in {
-        val matrix = new Matrix[Cell](2, Cell(""))
-        matrix.size should be(2)
+        val matrix = new Matrix[Cell](2, 2, Cell(""))
+        matrix.width should be(2)
       }
       "for test purposes only be created with a Vector of Vectors" in {
         val testMatrix = Matrix(Vector(Vector(Cell(""))))
-        testMatrix.size should be(1)
+        testMatrix.width should be(1)
       }
 
     }
     "filled" should {
-      val matrix = new Matrix[Cell](2, Cell(""))
+      val matrix = new Matrix[Cell](2, 2, Cell(""))
       "give access to its cells" in {
         matrix.cell(0, 0) should be(Cell(""))
       }
