74b6d4ac9cd17ac4378608e0ac79eb7a45597ef3
Helen <gaiserhelen@gmail.com>
2021-02-16 09:42:00 +0100

    case_

--- src/main/scala/de/htwg/se/stadtlandfluss/aview/Tui.scala
+++ src/main/scala/de/htwg/se/stadtlandfluss/aview/Tui.scala
@@ -7,10 +7,16 @@ class Tui {
   def processInputLine(input: String, grid:Grid):Grid = {
     input match {
       case "q" => grid
-      case "n" => new GridCreator(4, 8).createGrid(8, 4)
+      case "n" =>new GridCreator(4, 8).createGrid(8, 4)
+      case "Please enter rows" =>
+        new GridCreator(4, 8).createGrid(8, 4)
       case "s" =>
         new Solver().solveGame(grid)
-      case _ => {
+      case _ => { input.toList.filter(c => c != ' ').map(c => c.toString.toInt) match {
+        case row :: column :: value :: Nil => grid.set(row, column, value.toString)
+
+        case _ => grid
+      }
         grid
         }
       }
