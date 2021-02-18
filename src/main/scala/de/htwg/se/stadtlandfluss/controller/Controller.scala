98ba235e8ce0062f9b2942fbc53b6a7b9c6f3f0f
wolfbiker1 <be631wil@htwg-konstanz.de>
2021-02-18 15:21:36 +0100

    wip: work on undo manager

--- src/main/scala/de/htwg/se/stadtlandfluss/controller/Controller.scala
+++ src/main/scala/de/htwg/se/stadtlandfluss/controller/Controller.scala
@@ -1,9 +1,14 @@
 package de.htwg.se.stadtlandfluss.controller
 
 import de.htwg.se.stadtlandfluss.model.{Grid, GridCreator, Solver}
-import de.htwg.se.stadtlandfluss.util.Observable
+import de.htwg.se.stadtlandfluss.util.{Observable, UndoManager}
 
 class Controller(var grid: Grid) extends Observable {
+
+
+  private val undoManager = new UndoManager
+
+
   def createEmptyGrid(width: Int, height: Int): Unit = {
     grid = new Grid(width, height)
     notifyObservers
@@ -18,12 +23,28 @@ class Controller(var grid: Grid) extends Observable {
   def gridToString: String = grid.toString
 
   def set(row: Int, col: Int, value: String): Unit = {
-    grid = grid.set(row, col, value)
+//    grid = grid.set(row, col, value)
+    undoManager.doStep(new SetCommand(row, col, value, this))
     notifyObservers
   }
 
   def solve() = {
     grid = new Solver().solveGame(grid)
+    notifyObservers
+  }
+
+  def _solve: Unit = {
+//    undoManager.doStep(new SolveCommand(this))
+    notifyObservers
+  }
+
+  def undo: Unit = {
+    undoManager.undoStep
+    notifyObservers
+  }
+
+  def redo: Unit = {
+    undoManager.redoStep
     notifyObservers
   }
 
