540f73152740de27ff1cce57578931bf053a34d2
Helen <gaiserhelen@gmail.com>
2021-02-18 10:38:46 +0100

    bissle test

--- src/test/scala/de/htwg/se/stadtlandfluss/aview/TUISpec.scala
+++ src/test/scala/de/htwg/se/stadtlandfluss/aview/TUISpec.scala
@@ -12,10 +12,19 @@ class TuiSpec  extends WordSpec with Matchers{
     "do nothing on input 'q'" in {
       tui.processInputLine("q")
     }
-    //"create a Sudoku on input s'n-$i'" in {
-    //  tui.processInputLine("n-8")
-    //  controller.createRandomGrid(4,8,8,4) should be(new Grid(8,4))
-   // }
+ //   "create a Sudoku on input s'n-$i'" in {// todo ändern logik
+//      tui.processInputLine("n-8")
+ //     controller.createRandomGrid(4,8,8,4) should be(new Grid(8,4))
+  //  }
+    "set a cell on input '2,dk'" in {
+      tui.processInputLine("2,dk")
+      controller.grid.cell(1,1).value should be("dk") //2-1
+    }
+    "solve a Sudoku on input 's'" in {
+      tui.processInputLine("n-8")
+      tui.processInputLine("s")
+     controller.solve should be() // todo boolean logik
+    }
 
   }
 
