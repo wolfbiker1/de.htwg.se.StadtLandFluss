cd87f8dea37043c60a8aa2b8618fc434fc7a1e85
Helen <gaiserhelen@gmail.com>
2021-03-10 11:55:11 +0100

    test controller

--- src/test/scala/de/htwg/se/stadtlandfluss/controller/ControllerSpec.scala
+++ src/test/scala/de/htwg/se/stadtlandfluss/controller/ControllerSpec.scala
@@ -2,6 +2,7 @@ package de.htwg.se.stadtlandfluss.controller
 
 import de.htwg.se.stadtlandfluss.SLF.controller
 import de.htwg.se.stadtlandfluss.aview.Tui
+import de.htwg.se.stadtlandfluss.controller.controllerBaseImpl.Controller
 import de.htwg.se.stadtlandfluss.model.gridComponent.gridBaseImpl.Grid
 import de.htwg.se.stadtlandfluss.util.Observer
 import org.scalatest.{Matchers, WordSpec}
@@ -10,6 +11,31 @@ import scala.swing.Publisher
 
 class ControllerSpec extends WordSpec with Matchers {
 
+  "A Controller" when {
+    val controller = Controller.getController
+    val name = "heinrich" :: ("hans" :: "14" :: Nil)
+    "a new grid" should {
+      val smallGrid = new Grid(4, 4)
+      controller.createRandomGrid(4, 4)
+      "get the Amount of Columns" in {
+        controller.getAmountOfColumns should be(4)
+      }
+      "get the Number of Columns" in {
+        controller.getNumberOfColumns should be(4)
+      }
+      "get the Amount of Rows" in {
+        controller.getAmountOfRows should be(4)
+      }
 
+    }
 
+    " a grid with player" should {
+      controller.addPlayer(name)
+      controller.addPlayer(name)
+      " should be ready" in {
+        controller.isReady should be ( true)
+      }
+    }
+
+  }
 }
