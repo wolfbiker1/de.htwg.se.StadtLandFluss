4aa147de150086397e28f4705cac77d82b592541
wolfbiker1 <be631wil@htwg-konstanz.de>
2021-03-11 11:20:08 +0100

    feat: add file component

---
+++ src/main/scala/de/htwg/se/stadtlandfluss/model/fileIoComponent/FileIOInterface.scala
@@ -0,0 +1,9 @@
+package de.htwg.se.stadtlandfluss.model.fileIoComponent
+
+import de.htwg.se.stadtlandfluss.model.gridComponent.GridInterface
+
+trait FileIOInterface {
+  def load: GridInterface
+  def save(grid: GridInterface): Unit
+
+}
---
+++ src/main/scala/de/htwg/se/stadtlandfluss/model/fileIoComponent/fileIoJsonImpl/FileIo.scala
@@ -0,0 +1,10 @@
+package de.htwg.se.stadtlandfluss.model.fileIoComponent.fileIoJsonImpl
+
+import com.google.inject.Guice
+import com.google.inject.name.Names
+import net.codingwell.scalaguice.InjectorExtensions._
+import play.api.libs.json._
+
+class FileIo {
+
+}
